8d07b0cca78fcc4ebfe6dd0c5d7a419dd4143c59
SeanShih <seanshih@cw.com.tw>
2019-04-25 09:13:40 +0800

    FakeUser add recommendations relationship

--- tests/illuminate/User.php
+++ tests/illuminate/User.php
@@ -3,10 +3,16 @@
 namespace Tsubasarcs\Recommendations\Tests\Illuminate;
 
 use Illuminate\Database\Eloquent\Model;
+use Tsubasarcs\Recommendations\Recommendation;
 
 class User extends Model
 {
     protected $table = 'users';
 
     protected $guarded = [];
+
+    public function recommendations()
+    {
+        return $this->hasMany(Recommendation::class);
+    }
 }
